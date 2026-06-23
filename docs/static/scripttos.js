4b4be45c85e28cab47ce0fbcbcdea6e88038c3f7
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-06-24 06:42:44 +0700

    Change

--- docs/static/scripttos.js
+++ docs/static/scripttos.js
@@ -6,23 +6,29 @@ let popup = document.getElementById("popup");
 let open = document.getElementById("open");
 let close = document.getElementById("close");
 let pooop = document.querySelector(".popup-content");
+let txt = document.querySelector(".txt")
 const mousmoov = document.querySelector(".mousmoov")
 
 let random = Math.floor(Math.random() * (max - min + 1)) + min;
 
 if (random == 1) {
     ok.innerHTML = "WHITE - WOLF TOOLS | TOS";
-    setInterval(() => {
-        let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-        ok.style.color = randomColor;
-    }, 1000);
+    // setInterval(() => {
+    //     let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    //     ok.style.color = randomColor;
+    // }, 1000);
 } else {
     ok.innerHTML = "TOS - WHITE WOLF TOOLS";
-    setInterval(() => {
-        let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-        ok.style.color = randomColor;
-    }, 1000);
+    // setInterval(() => {
+    //     let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    //     ok.style.color = randomColor;
+    // }, 1000);
 }
+
+setInterval(() => {
+    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    txt.style.borderColor = randomColor;
+}, 1000);
 
 btn.addEventListener("click", function (e) {
     window.location.href = "index.html"
